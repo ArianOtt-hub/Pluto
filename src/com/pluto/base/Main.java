ed3361a1a26be8b502b50fd82000bf07437565ff
ottar <arian.ott@thunderlight.de>
2022-05-11 19:03:38 +0200

    v 1.0.0 Bot ground structure

--- src/com/pluto/base/Main.java
+++ src/com/pluto/base/Main.java
@@ -3,10 +3,12 @@ package com.pluto.base;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
 public class Main {
     public static JDA jda;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LoginException {
 
-	jda = JDABuilder.createDefault()
+	jda = JDABuilder.createDefault(Token.getToken()).build();
     }
 }
