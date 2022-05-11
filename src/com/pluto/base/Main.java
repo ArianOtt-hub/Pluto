8e5c15133e3f1e00c86a007842cee8781b6f1cdb
ottar <arian.ott@thunderlight.de>
2022-05-11 19:00:29 +0200

    Initial commit

---
+++ src/com/pluto/base/Main.java
@@ -0,0 +1,12 @@
+package com.pluto.base;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+    public static JDA jda;
+    public static void main(String[] args) {
+
+	jda = JDABuilder.createDefault()
+    }
+}
