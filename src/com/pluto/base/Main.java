5df8d0529cb012f072ddd0815988526306a45722
ottar <arian.ott@thunderlight.de>
2022-05-11 19:42:41 +0200

    v 1.1.0 Connection with the Listener done Bot can listen to different commands

--- src/com/pluto/base/Main.java
+++ src/com/pluto/base/Main.java
@@ -1,14 +1,21 @@
 package com.pluto.base;
 
+import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.JDAInfo;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class Main {
+public class Main extends ListenerAdapter {
     public static JDA jda;
-    public static void main(String[] args) throws LoginException {
+    public static  String prefix = "!";
 
-	jda = JDABuilder.createDefault(Token.getToken()).build();
+    public static void main(String[] args) throws LoginException, InterruptedException {
+
+	jda = JDABuilder.createDefault(Token.getToken()).build().awaitReady();
+jda.addEventListener(new Commands());
     }
 }
